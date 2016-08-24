2d56f37b2753051a0e5f0b96967c762075cf2d1f
Rohit <rohitdotacustom@gmail.com>
2016-08-24 01:02:11 -0400

    Add file for round structure

---
+++ game/dota_addons/Barebones-TurnBased/scripts/vscripts/round.lua
@@ -0,0 +1,13 @@
+function Round:StartRound()
+	-- do things for the round here
+
+	-- spawn heroes with items from tracking
+	-- initialize timers
+end
+
+function Round:EndRound()
+	-- do things to end the round here
+
+	-- despawn heroes while tracking their items
+	-- delete all timers
+end
